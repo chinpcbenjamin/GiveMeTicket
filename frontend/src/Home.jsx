1ebfb50f6617f39ac6b1b71c9490f08328a44009
chinpcbenjamin <chinpcbenjamin@gmail.com>
2026-03-31 00:03:44 +0800

    add contract to frontend

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+// import { ethers } from "ethers";
+// import { getContract } from "../contract/useContract";
+
+
 export default function Home() {
+    const [ticketPrice, setTicketPrice] = useState(5); //for now i put 5 just as a test. need to integrate with backend later on
+
+    // useEffect(() => {
+    //     async function fetchPrice() {
+    //         const contract = await getContract();
+    //         const price = await contract.getTicketPrice(some_token_we_need_to_change);
+    //         setTicketPrice(ethers.formatEther(price));
+    //     }
+    //     fetchPrice();
+    // }, []);
+
     return (
         <div className="min-h-screen min-w-sc bg-white flex flex-col items-center py-20 px-4">
             <h1 className="text-5xl font-extrabold tracking-tight mb-16">
@@ -7,7 +23,7 @@ export default function Home() {
 
             <div className="flex flex-col gap-6 items-center">
                 <div className="bg-black transition cursor-pointer rounded-2xl p-10 w-100 flex items-center justify-center">
-                    <p className="text-xl text-white font-bold">Current Ticket Price:</p>
+                    <p className="text-xl text-white font-bold">Current Ticket Price: {ticketPrice} ETH</p>
                 </div>
 
                 <div className="bg-blue-400 hover:bg-blue-600 transition cursor-pointer rounded-2xl p-10 w-56 flex items-center justify-center">
