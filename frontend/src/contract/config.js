8a8fb083053c17c136c2064f9b3c94695f1aa00a
chinpcbenjamin <chinpcbenjamin@gmail.com>
2026-04-06 22:23:31 +0800

    update interfaces

--- frontend/src/contract/config.js
+++ frontend/src/contract/config.js
@@ -2,8 +2,8 @@ import TicketingPlatformArtifact from "../../../artifacts/contracts/TicketingPla
 import MarketplaceArtifact from "../../../artifacts/contracts/Marketplace.sol/Marketplace.json";
 import addresses from "./deployed-addresses.json";
 
-export const CONTRACT_ADDRESS = addresses.ticketingPlatform;
+export const TICKETING_PLATFORM_ADDRESS = addresses.ticketingPlatform;
 export const MARKETPLACE_ADDRESS = addresses.marketplace;
 
-export const ABI = TicketingPlatformArtifact.abi;
+export const TICKETING_PLATFORM_ABI = TicketingPlatformArtifact.abi;
 export const MARKETPLACE_ABI = MarketplaceArtifact.abi;
